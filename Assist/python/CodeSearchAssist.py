0c49653971688b4891f1cf390d2bd7aff85826ee
Yu Xie <yxie@opera.com>
2013-03-27 18:43:49 +0800

    draft

--- Assist/python/CodeSearchAssist.py
+++ Assist/python/CodeSearchAssist.py
@@ -7,6 +7,8 @@ from Common import CommonUtil
 
 class CodeSearchAssist:
     searchDir = None
+    storeFileName = "codeSearchHistory"
+    searchHistory = None
     #public interface:
     @staticmethod
     def search(symbol):
@@ -49,4 +51,75 @@ class CodeSearchAssist:
         absPath = os.path.abspath(dir)
         cmd = "cindex %s" % absPath
         subprocess.Popen(cmd, shell=True)
+
+    @staticmethod
+    def addToSearchHistory(iterm):
+        if CodeSearchAssist.searchHistory is None:
+            CodeSearchAssist.searchHistory = CodeSearchAssist.load()
+
+        for i, b in enumerate(CodeSearchAssist.searchHistory):
+            if b.equal(iterm):
+                del CodeSearchAssist.searchHistoryi]
+                break
+        CodeSearchAssist.searchHistory.append(iterm)
+        CodeSearchAssist.save()
+
+    @staticmethod
+    def load():
+        storeFilePath = os.path.join(SettingManager.getStoreDir(), CodeSearchAssist.storeFileName)
+        try:
+            f = open(storeFilePath, 'r')
+            for line in f.readlines():
+                iterm = CodeSearchHistoryIterm.CreateInstancefromString(line.strip())
+                if iterm:
+                    result.append(iterm)
+        except:
+            pass
+        return result
+
+    @staticmethod
+    def save():
+        storeFilePath = os.path.join(SettingManager.getStoreDir(), CodeSearchAssist.storeFileName)
+        f = open(storeFilePath, 'w')
+        for b in CodeSearchAssist.searchHistory:
+            f.write("%s\n" % b.toString())
+        f.close();
+        pass
+
+    @staticmethod
+    def getSearchHistory():
+        pass
+
+class CodeSearchHistoryIterm(SearchIterm):
+    def __init__(self, searchDir, symbolName):
+        self.searchDir = searchDir
+        self.symbolName = symbolName
+
+    def displayText(self):
+        if self.searchDir:
+            return "%-30s\t%s" %(self.symbolName, self.searchDir)
+        else:
+            return "%-30s" % (self.symbolName)
+
+    def onAction(self, action):
+        return True
+
+    def getRankKey(self):
+        return symbolName
+
+    @staticmethod
+    def CreateInstancefromString(s):
+        ret = None
+        try:
+            pattern = re.compile("(\S*)\s*(.*$)")
+            line = s.strip()
+            if line:
+                (symbolName, searchDir) = pattern.search(line).groups()
+                ret = CodeSearchHistoryIterm(searchDir, symbolName)
+
+    def toString(self):
+        return self.displayText
+
+    def equal(self, iterm):
+        pass
 
